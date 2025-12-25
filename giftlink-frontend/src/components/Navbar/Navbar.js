a945cdbfeb6f70049ed7803b78a42e1d2ad89c0c
linhnguyen15492 <84197075+linhnguyen15492@users.noreply.github.com>
2025-12-25 14:26:46 +0700

    add frontend project

---
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="/">
+        GiftLink
+      </a>
+
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="/home.html">
+              Home
+            </a>{" "}
+          </li>
+
+          <li className="nav-item">
+            <a className="nav-link" href="/app">
+              Gifts
+            </a>{" "}
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
