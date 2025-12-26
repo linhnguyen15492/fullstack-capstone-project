1864e330ca74850464e6a363bfb00d020d60b224
linhnguyen15492 <honglinh154@gmail.com>
2025-12-26 15:01:07 +0700

    style: lint code

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -1,43 +1,45 @@
+/*jshint esversion: 8 */
+
 // Import necessary packages
-const express = require("express");
-const app = express();
-const bcryptjs = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const connectToDatabase = require("../models/db");
-const router = express.Router();
-const dotenv = require("dotenv");
-const pino = require("pino"); // Import Pino logger
+const express = require('express')
+const app = express()
+const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const connectToDatabase = require('../models/db')
+const router = express.Router()
+const dotenv = require('dotenv')
+const pino = require('pino') // Import Pino logger
 
 // Create a Pino logger instance
-const logger = pino(); // Create a Pino logger instance
+const logger = pino() // Create a Pino logger instance
 
-dotenv.config();
+dotenv.config()
 
-const { body, validationResult } = require("express-validator");
+const { body, validationResult } = require('express-validator')
 
 // Create JWT secret
-const JWT_SECRET = process.env.JWT_SECRET;
-const collection_name = "users";
+const JWT_SECRET = process.env.JWT_SECRET
+const collection_name = 'users'
 
-router.post("/register", async (req, res) => {
+router.post('/register', async (req, res) => {
   try {
     // Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`
-    const db = await connectToDatabase();
+    const db = await connectToDatabase()
 
     // Access MongoDB collection
-    const collection = db.collection(collection_name);
+    const collection = db.collection(collection_name)
 
     // Check for existing email
-    const existingEmail = await collection.findOne({ email: req.body.email });
+    const existingEmail = await collection.findOne({ email: req.body.email })
 
     if (existingEmail) {
-      logger.error("Email id already exists");
-      return res.status(400).json({ error: "Email id already exists" });
+      logger.error('Email id already exists')
+      return res.status(400).json({ error: 'Email id already exists' })
     }
 
-    const salt = await bcryptjs.genSalt(10);
-    const hash = await bcryptjs.hash(req.body.password, salt);
-    const email = req.body.email;
+    const salt = await bcryptjs.genSalt(10)
+    const hash = await bcryptjs.hash(req.body.password, salt)
+    const email = req.body.email
 
     // Save user details in database
     const newUser = await collection.insertOne({
@@ -46,132 +48,132 @@ router.post("/register", async (req, res) => {
       lastName: req.body.lastName,
       password: hash,
       createdAt: new Date(),
-    });
+    })
 
     // Create JWT authentication with user._id as payload
     const payload = {
       user: {
         id: newUser.insertedId,
       },
-    };
+    }
 
-    const authtoken = jwt.sign(payload, JWT_SECRET);
-    logger.info("User registered successfully");
-    return res.status(201).json({ authtoken, email });
+    const authtoken = jwt.sign(payload, JWT_SECRET)
+    logger.info('User registered successfully')
+    return res.status(201).json({ authtoken, email })
   } catch (e) {
-    return res.status(500).send("Internal server error");
+    return res.status(500).send('Internal server error')
   }
-});
+})
 
-router.post("/login", async (req, res) => {
+router.post('/login', async (req, res) => {
   try {
     // Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`.
-    const db = await connectToDatabase();
+    const db = await connectToDatabase()
 
     // Access MongoDB `users` collection
-    const collection = db.collection(collection_name);
+    const collection = db.collection(collection_name)
 
     // Check for user credentials in database
-    const existingUser = await collection.findOne({ email: req.body.email });
-    console.log(existingUser);
+    const existingUser = await collection.findOne({ email: req.body.email })
+    console.log(existingUser)
 
     // Check if the password matches the encrypyted password and send appropriate message on mismatch
     if (existingUser) {
       const passwordMatch = await bcryptjs.compare(
         req.body.password,
         existingUser.password
-      );
+      )
 
       if (!passwordMatch) {
-        logger.error("Invalid credentials");
-        return res.status(401).send("Invalid credentials");
+        logger.error('Invalid credentials')
+        return res.status(401).send('Invalid credentials')
       }
 
       // Fetch user details from database
-      const userName = existingUser.firstName;
-      const userEmail = existingUser.email;
+      const userName = existingUser.firstName
+      const userEmail = existingUser.email
 
       // Create JWT authentication if passwords match with user._id as payload
       let payload = {
         user: {
           id: existingUser._id.toString(),
         },
-      };
+      }
 
-      const authtoken = jwt.sign(payload, JWT_SECRET);
-      logger.info("User logged in successfully");
-      return res.status(200).json({ authtoken, userName, userEmail });
+      const authtoken = jwt.sign(payload, JWT_SECRET)
+      logger.info('User logged in successfully')
+      return res.status(200).json({ authtoken, userName, userEmail })
     } else {
       // Send appropriate message if user not found
-      logger.error("User not found");
-      return res.status(404).json({ error: "User not found" });
+      logger.error('User not found')
+      return res.status(404).json({ error: 'User not found' })
     }
   } catch (e) {
-    return res.status(500).send("Internal server error");
+    return res.status(500).send('Internal server error')
   }
-});
+})
 
-router.put("/update", async (req, res) => {
+router.put('/update', async (req, res) => {
   // Validate the input using `validationResult` and return approiate message if there is an error.
-  const errors = validationResult(req);
+  const errors = validationResult(req)
 
   if (!errors.isEmpty()) {
-    logger.error("Validation errors in update request", errors.array());
-    return res.status(400).json({ errors: errors.array() });
+    logger.error('Validation errors in update request', errors.array())
+    return res.status(400).json({ errors: errors.array() })
   }
 
   try {
     // Check if `email` is present in the header and throw an appropriate error message if not present.
-    const email = req.headers.email;
+    const email = req.headers.email
 
     if (!email) {
-      logger.error("Email not found in the request headers");
+      logger.error('Email not found in the request headers')
       return res
         .status(400)
-        .json({ error: "Email not found in the request headers" });
+        .json({ error: 'Email not found in the request headers' })
     }
 
     // Connect to MongoDB
-    const db = await connectToDatabase();
-    const collection = db.collection("users");
+    const db = await connectToDatabase()
+    const collection = db.collection('users')
 
     // find user credentials in database
-    const existingUser = await collection.findOne({ email });
+    const existingUser = await collection.findOne({ email })
 
     if (!existingUser) {
-      logger.error("User not found");
-      return res.status(404).json({ error: "User not found" });
+      logger.error('User not found')
+      return res.status(404).json({ error: 'User not found' })
     }
 
     // const passwordMatch = await bcryptjs.compare(
     //   req.body.oldPassword,
     //   existingUser.password
-    // );
+    // )
 
-    existingUser.firstName = req.body.name;
-    existingUser.updatedAt = new Date();
+    existingUser.firstName = req.body.name
+    existingUser.updatedAt = new Date()
 
     // update user credentials in database
     const updatedUser = await collection.findOneAndUpdate(
       { email },
       { $set: existingUser },
-      { returnDocument: "after" }
-    );
+      { returnDocument: 'after' }
+    )
 
     // create JWT authentication using secret key from .env file
     const payload = {
       user: {
         id: updatedUser._id.toString(),
       },
-    };
+    }
 
-    const authtoken = jwt.sign(payload, JWT_SECRET);
-    logger.info("User updated successfully");
-    return res.status(200).json({ authtoken });
+    const authtoken = jwt.sign(payload, JWT_SECRET)
+    logger.info('User updated successfully')
+    return res.status(200).json({ authtoken })
   } catch (e) {
-    logger.error("Error updating user", e);
-    return res.status(500).send("Internal server error");
+    logger.error('Error updating user', e)
+    return res.status(500).send('Internal server error')
   }
-});
+})
 
-module.exports = router;
+module.exports = router
--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,66 +1,67 @@
-const express = require("express");
-const connectToDatabase = require("../models/db");
-const logger = require("../logger");
-const router = express.Router();
+/*jshint esversion: 8 */
+const express = require('express')
+const connectToDatabase = require('../models/db')
+const logger = require('../logger')
+const router = express.Router()
 
-const collection_name = "gifts";
+const collection_name = 'gifts'
 
-router.get("/", async (req, res) => {
-  logger.info("/ called");
+router.get('/', async (req, res) => {
+  logger.info('/ called')
 
   try {
     // Connect to MongoDB and store connection to db constant
-    const db = await connectToDatabase();
+    const db = await connectToDatabase()
 
     // use the collection() method to retrieve the gift collection
-    const collection = db.collection(collection_name);
+    const collection = db.collection(collection_name)
 
     // Fetch all gifts using the collection.find method. Chain with toArray method to convert to JSON array
-    const gifts = await collection.find({}).toArray();
+    const gifts = await collection.find({}).toArray()
 
     // return the gifts using the res.json method
-    return res.status(200).json(gifts);
+    return res.status(200).json(gifts)
   } catch (e) {
-    logger.console.error("Error fetching gifts:", e);
-    return res.status(500).send("Error fetching gifts");
+    logger.console.error('Error fetching gifts:', e)
+    return res.status(500).send('Error fetching gifts')
   }
-});
+})
 
-router.get("/:id", async (req, res) => {
+router.get('/:id', async (req, res) => {
   try {
     // Connect to MongoDB and store connection to db constant
-    const db = await connectToDatabase();
+    const db = await connectToDatabase()
 
     // use the collection() method to retrieve the gift collection
-    const collection = db.collection(collection_name);
+    const collection = db.collection(collection_name)
 
-    const id = req.params.id;
+    const id = req.params.id
 
     // Find a specific gift by ID using the collection.fineOne method and store in constant called gift
-    const gift = await collection.findOne({ id: id });
+    const gift = await collection.findOne({ id: id })
 
     if (!gift) {
-      return res.status(404).send("Gift not found");
+      return res.status(404).send('Gift not found')
     }
 
-    return res.status(200).json(gift);
+    return res.status(200).json(gift)
   } catch (e) {
-    console.error("Error fetching gift:", e);
-    return res.status(500).send("Error fetching gift");
+    console.error('Error fetching gift:', e)
+    return res.status(500).send('Error fetching gift')
   }
-});
+})
 
 // Add a new gift
-router.post("/", async (req, res, next) => {
+router.post('/', async (req, res, next) => {
   try {
-    const db = await connectToDatabase();
-    const collection = db.collection(collection_name);
-    const gift = await collection.insertOne(req.body);
+    const db = await connectToDatabase()
+    const collection = db.collection(collection_name)
+    const gift = await collection.insertOne(req.body)
 
-    return res.status(201).json(gift.ops[0]);
+    return res.status(201).json(gift.ops[0])
   } catch (e) {
-    next(e);
+    next(e)
   }
-});
+})
 
-module.exports = router;
+module.exports = router
